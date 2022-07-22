e83b159ec8fe0ac9a046c49a9f8af6474414c9b6
Jan <jan@macbook-m1-pro-air-max>
2022-07-22 12:00:23 +0200

    Update Footer.tsx

--- public/app/core/components/Footer/Footer.tsx
+++ public/app/core/components/Footer/Footer.tsx
@@ -1,5 +1,35 @@
 import React, { FC } from 'react';
 
+import { config } from '@grafana/runtime';
+
+
+export interface FooterLink {
+  text: string;
+  id?: string;
+  url?: string;
+  target?: string;
+}
+
+export let getFooterLinks = (): FooterLink[] => {
+  return [
+    {
+      text: 'Documentation',
+      url: 'https://grafana.com/docs/grafana/latest/?utm_source=grafana_footer',
+      target: '_blank',
+    },
+    {
+      text: 'Support',
+      url: 'https://grafana.com/products/enterprise/?utm_source=grafana_footer',
+      target: '_blank',
+    },
+    {
+      text: 'Community',
+      url: 'https://community.grafana.com/?utm_source=grafana_footer',
+      target: '_blank',
+    },
+  ];
+};
+
 export const Footer: FC = React.memo(() => {
 
   return (
